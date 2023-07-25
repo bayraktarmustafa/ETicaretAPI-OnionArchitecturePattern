6393048fb9fc870fd3a638aff7b6a6fd2551a0e6
bayraktarmustafa <m.bayraktar01@hotmail.com>
2023-07-26 00:14:51 +0300

    Migrations are completed

---
+++ Infrastructure/Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+	public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<APIDbContext>
+	{
+		public APIDbContext CreateDbContext(string[] args)
+		{
+			
+			DbContextOptionsBuilder<APIDbContext> dbContextOptionsBuilder = new();
+
+			dbContextOptionsBuilder.UseNpgsql(Configuration.ConnectionString);
+
+			return new APIDbContext(dbContextOptionsBuilder.Options);
+
+		}
+	}
+}
